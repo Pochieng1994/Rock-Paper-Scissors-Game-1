c6608765d874b6e0d663a346c2883d6a5aa57c36
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-05-10 18:41:55 -0400

    Finished Rock Paper Scissors game pushing to Github

---
+++ script.js
@@ -0,0 +1,73 @@
+playerChoiceDisplay = document.querySelector('#player-choice');
+computerChoiceDisplay = document.querySelector('#computer-choice');
+result = document.querySelector('#result');
+buttons = document.querySelectorAll('button');
+let playerChoice;
+let computerChoice;
+
+
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    playerChoice = e.target.id;
+    playerChoiceDisplay.textContent = playerChoice;
+    generatedComputerChoice();
+    getResult();
+  })
+})
+
+function generatedComputerChoice () {
+  randomNumber = Math.ceil(Math.random() * 3);
+
+  if (randomNumber === 1) {
+    computerChoice = 'rock';
+  }
+
+  if (randomNumber === 2) {
+    computerChoice = 'paper';
+  }
+
+  if (randomNumber === 3) {
+    computerChoice = 'scissors';
+  }
+
+  computerChoiceDisplay.textContent = computerChoice;
+}
+
+function getResult() {
+  if(playerChoice === computerChoice) {
+    result.textContent = 'Its a Tie!'
+  }
+
+  if(playerChoice === 'rock' && computerChoice === 'scissors'){
+    result.textContent = 'Hooray You Won!'
+  }
+
+  if(playerChoice === 'rock' && computerChoice === 'paper'){
+    result.textContent = 'You Lost'
+  }
+
+  if(playerChoice === 'scissors' && computerChoice === 'rock'){
+    result.textContent = 'You Lost'
+  }
+
+  if(playerChoice === 'scissors' && computerChoice === 'paper'){
+    result.textContent = 'Hooray You Won!'
+  }
+
+  if(playerChoice === 'paper' && computerChoice === 'scissors'){
+    result.textContent = 'You Lost'
+  }
+
+  if(playerChoice === 'paper' && computerChoice === 'rock'){
+    result.textContent = 'Hooray You Won!'
+  }
+
+}
+
+
+
+
+
+
+
